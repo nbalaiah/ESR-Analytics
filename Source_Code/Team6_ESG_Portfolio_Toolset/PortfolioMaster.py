6f57dd21b61cfbefb989eef9b545f71b5931e523
Rajavel <rajavel.jp@gmail.com>
2023-03-22 17:09:04 +0530

    dataframe rendering using DataTable.js

--- Source_Code/Team6_ESG_Portfolio_Toolset/PortfolioMaster.py
+++ Source_Code/Team6_ESG_Portfolio_Toolset/PortfolioMaster.py
@@ -13,20 +13,29 @@ def show():
 @app.route('/portfolio/<name>')
 def show_portfolio(name):
    basedir = os.path.abspath(os.path.dirname(__file__))
-   data_file = os.path.join(basedir, 'data/' + name + '.csv')
-   portfolio = pd.read_csv(data_file)
+   portfolio_file = os.path.join(basedir, 'data/' + name + '.csv')
+   benchmark_file = os.path.join(basedir, 'data/benchmark.csv')
+   portfolio = pd.read_csv(portfolio_file)
+   benchmark = pd.read_csv(benchmark_file)
    portfolio['CreatedDate']= pd.to_datetime(portfolio['CreatedDate'])
+   benchmark['CreatedDate']= pd.to_datetime(benchmark['CreatedDate'])
    maxDate = portfolio['CreatedDate'].max()
    minDate = portfolio['CreatedDate'].min()
    tickers = portfolio['Ticker'].unique()
    data = pd.DataFrame()
+   data_benchmark = pd.DataFrame()
+   invested_value_bench = benchmark.query('CreatedDate ==\'' + str(minDate) + '\'')['Invested_Value'].iloc[0]
+   current_value_bench = benchmark.query('CreatedDate ==\'' + str(maxDate) + '\'')['Invested_Value'].iloc[0]
+   data_benchmark = data_benchmark.append({'Invested_Value':invested_value_bench,'Current_Value':current_value_bench},ignore_index=True)
    for ticker in tickers:
         invested_value = portfolio.query('CreatedDate ==\'' + str(minDate) + '\' and Ticker ==\'' + ticker + '\'')['Invested_Value'].iloc[0]
         current_value = portfolio.query('CreatedDate ==\'' + str(maxDate) + '\' and Ticker ==\'' + ticker + '\'')['Invested_Value'].iloc[0]
         climate_value = portfolio.query('CreatedDate ==\'' + str(maxDate) + '\' and Ticker ==\'' + ticker + '\'')['Climate'].iloc[0]
         data = data.append({'Ticker':ticker,'Invested_Value': invested_value,'Current_Value':current_value,'Climate': climate_value},ignore_index=True)
-   
-   return render_template('portfolio.html', tables=[data.to_html()], titles=[''])
+   #data=x.to_dict(orient='records')
+   #return render_template('portfolio.html', tables=[data.to_html(),data_benchmark.to_html()], titles=['Portfolio', 'Benchmark'])
+   return render_template('portfolio.html', portfolio_data=data.to_dict(orient='records'),title='ESG Portfolio')
+
 
    #return jsonify(WORDS)
     #return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
