9096744bacd3e2bfe37f5a1421162cf90f1cd965
Rajavel <rajavel.jp@gmail.com>
2023-03-30 09:38:22 +0530

    -

--- Source_Code/Team6_ESG_Portfolio_Toolset/app/rest.py
+++ Source_Code/Team6_ESG_Portfolio_Toolset/app/rest.py
@@ -27,5 +27,5 @@ class Portfolios(Resource):
       return jsonify(portfolios)
 
 
-if __name__ == '__main__':
-    app.run(debug = True, port = 5001)
+#if __name__ == '__main__':
+ #   app.run(debug = True, port = 5001)
